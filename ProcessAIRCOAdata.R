afd7d50f16b451c7f7e35158440614c85422c5b8
davidjpmoore <davidjpmoore@arizona.edu>
2015-03-31 06:58:11 -0700

    Initial Commit - AIRCOA processing scripts

---
+++ ProcessAIRCOAdata.R
@@ -0,0 +1,5 @@
+#purpose: Calculate diurnal change in CO2 concentration at FEF after rise in atmospheric background has been removed by substracting observations from SPL http://www.eol.ucar.edu/homes/stephens/RACCOON/index.html
+#author: Dave Moore
+#Date: 03/31/2015
+
+load (file="Aircoa.rdata")
---
+++ ReadAIRCOAdata.R
@@ -0,0 +1,11 @@
+#purpose: read AIRCOA data from FEF and SPL http://www.eol.ucar.edu/homes/stephens/RACCOON/index.html
+#author: Dave Moore
+#Date: 03/31/2015
+
+#data stored locally on my machine
+FEFACO = read.table("AIRCOA//rdNCAR_FEF_most_recent.lin03312015" ,header=TRUE, stringsAsFactors = FALSE)
+
+SPLACO = read.table("AIRCOA//rdNCAR_SPL_most_recent.lin03312015" ,header=TRUE, stringsAsFactors = FALSE)
+
+#store data in .Rdata file
+save (FEFACO,SPLACO, file="Aircoa.rdata")
