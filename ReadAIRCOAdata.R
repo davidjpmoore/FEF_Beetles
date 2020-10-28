e97f4d05f5f89eb29e715e6ded62d0a6860abc14
davidjpmoore <davidjpmoore@email.arizona.edu>
2020-10-28 14:51:38 -0600

    Updated required file paths

--- ReadAIRCOAdata.R
+++ ReadAIRCOAdata.R
@@ -3,9 +3,15 @@
 #Date: 03/31/2015
 
 #data stored locally on my machine
-FEFACO = read.table("AIRCOA//rdNCAR_FEF_most_recent.lin03312015" ,header=TRUE, stringsAsFactors = FALSE)
+FEFACO = read.table("AIRCOA//rdNCAR_FEF_most_recent.lin03312015" ,header=TRUE, stringsAsFactors = FALSE, na.strings=c('-999.990'))
 
-SPLACO = read.table("AIRCOA//rdNCAR_SPL_most_recent.lin03312015" ,header=TRUE, stringsAsFactors = FALSE)
+FEFACO$CO21[FEFACO$CO21==-999.990]=NA
+
+SPLACO = read.table("AIRCOA//rdNCAR_SPL_most_recent.lin03312015" ,header=TRUE, stringsAsFactors = FALSE,  na.strings=c('-999.990'))
+
+SPLACO$CO21[SPLACO$CO21==-999.990]=NA
+
 
 #store data in .Rdata file
 save (FEFACO,SPLACO, file="Aircoa.rdata")
+
